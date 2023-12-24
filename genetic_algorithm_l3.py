8eedda39dab5bab5a93fea81527efb02356bad62
iamgirya <84345515+iamgirya@users.noreply.github.com>
2023-12-24 11:47:01 +0300

    Final lr8

--- genetic_algorithm_l3.py
+++ genetic_algorithm_l3.py
@@ -2,7 +2,15 @@ from random import uniform, random
 
 
 class GeneticAlgorithmL3:
-    def __init__(self, func, generations=50, min=True, mut_chance=0.8, survive_cof=0.8, pop_number=100):
+    def __init__(
+        self,
+        func,
+        generations=50,
+        min=True,
+        mut_chance=0.8,
+        survive_cof=0.8,
+        pop_number=100,
+    ):
         self.func = func
         self.population = dict()
         self.mut_chance = mut_chance
@@ -15,20 +23,31 @@ class GeneticAlgorithmL3:
         for i in range(self.pop_number):
             po_x = uniform(-x, x)
             po_y = uniform(-y, y)
-            self.population[i] = [po_x, po_y, self.func(po_x, po_y)]  # Создание начальной популяции
+            self.population[i] = [
+                po_x,
+                po_y,
+                self.func(po_x, po_y),
+            ]  # Создание начальной популяции
             # 2 гена (x, y) и значение фитнес-функции(z)
 
     def statistic(self):
-        return [max(self.population.items(), key=lambda item: item[1][2]),
-                min(self.population.items(), key=lambda item: item[1][2])]
+        return [
+            max(self.population.items(), key=lambda item: item[1][2]),
+            min(self.population.items(), key=lambda item: item[1][2]),
+        ]
 
     def select(self):
         sorted_pop = dict(
-            sorted(self.population.items(), key=lambda item: item[1][2], reverse=self.min_func))  # Ранжирование
+            sorted(
+                self.population.items(),
+                key=lambda item: item[1][2],
+                reverse=self.min_func,
+            )
+        )  # Ранжирование
 
         cof = int(self.pop_number * (1 - self.survive_cof))
-        parents1 = list(sorted_pop.items())[cof: cof * 2]
-        parents2 = list(sorted_pop.items())[self.pop_number - cof: self.pop_number]
+        parents1 = list(sorted_pop.items())[cof : cof * 2]
+        parents2 = list(sorted_pop.items())[self.pop_number - cof : self.pop_number]
 
         i = 0
         for pop in sorted_pop.values():
@@ -49,7 +68,11 @@ class GeneticAlgorithmL3:
     def mutation(self, cur_gen):
         for pop in self.population.values():
             if random() < self.mut_chance:
-                pop[0] += (random() - 0.5) * ((self.generations - cur_gen) / self.generations)
+                pop[0] += (random() - 0.5) * (
+                    (self.generations - cur_gen) / self.generations
+                )
             if random() < self.mut_chance:
-                pop[1] += (random() - 0.5) * ((self.generations - cur_gen) / self.generations)
+                pop[1] += (random() - 0.5) * (
+                    (self.generations - cur_gen) / self.generations
+                )
             pop[2] = self.func(pop[0], pop[1])
